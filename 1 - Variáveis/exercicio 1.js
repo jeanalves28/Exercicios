e8efd9de1d76cd32b50ed34d6f258d7717552c54
Jean Alves <j_alves_19@hotmail.com>
2022-08-27 19:53:53 -0400

    criando e manipulando variáveis em JavaScript

---
+++ 1 - Variáveis/exercicio 1.js
@@ -0,0 +1,12 @@
+const myName = 'Jean', birthCity = 'Manaus';
+let birthYear = 1994;
+
+console.log(myName,birthCity,birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = 'Manacapuru';
+
+console.log(birthCity);
