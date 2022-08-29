4cf200466caed062a6bb40609b2f56fb94711cc4
Jean Alves <j_alves_19@hotmail.com>
2022-08-28 22:55:14 -0400

    aprendendo a manipular algoritmos com preposições lógicas

---
+++ 4 - Operadores lógicos/Operador lógico OR/exercicio 5.js
@@ -0,0 +1,9 @@
+let weekDay = 'Sabado';
+
+weekDay = weekDay.toLowerCase();
+
+if(weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') 
+    console.log('Oba, mais um dia de aprendizado na Trybe >:D');
+
+if (weekDay === 'sabado' || weekDay === 'domingo')
+    console.log('FINALMENTE, descanso merecido UwU');
