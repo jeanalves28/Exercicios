3fbbb75224e500bb31fbdc5ba375afc8cc859d95
Jean Alves <j_alves_19@hotmail.com>
2022-09-18 21:56:57 -0400

    parte 2: criar e adicionar o botão feriado

--- exercício do bloco/script.js
+++ exercício do bloco/script.js
@@ -10,6 +10,12 @@ function createDaysOfTheWeek() {
     weekDaysList.appendChild(dayListItem);
   };
 };
+  
+createDaysOfTheWeek();
+decemberDaysList();
+buttonHoliday('Feriados');
+  
+  // Escreva seu código abaixo.
 
 function decemberDaysList() {
   const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
@@ -32,8 +38,14 @@ function decemberDaysList() {
     ulDays.appendChild(tagLi);
   }
 }
+
+function buttonHoliday(holiday) {
+  const tagButton = document.createElement('button');
+  const tagDivButton = document.querySelector('.buttons-container');
   
-createDaysOfTheWeek();
-decemberDaysList();
-  
-  // Escreva seu código abaixo.
+  tagButton.innerHTML = holiday;
+
+  tagButton.setAttribute('id', 'btn-holiday');
+
+  tagDivButton.appendChild(tagButton);
+}
