b7c07b1e2f2d17a956545da9dccf047f677fc880
Jean Alves <j_alves_19@hotmail.com>
2022-09-25 22:40:35 -0400

    fazendo algumas aplicações com o bootstrap

---
+++ README.md
@@ -0,0 +1,5 @@
+# Exercício do bloco 6.2
+
+## Utilizando *Frameworks* para **CSS**
+
+* Adicione o *framework* **Bootstrap** ao formulário que você construiu na última aula e o utilize para estilizar o formulário.
---
+++ script.js
@@ -0,0 +1,44 @@
+const buttonSubmit = document.getElementById('submit-form');
+const buttonClear = document.createElement('button');
+
+buttonSubmit.addEventListener('click',e => {
+    const inputAgreement = document.getElementById('agree');
+
+    if (inputAgreement.checked == false)
+        e.preventDefault();
+});
+
+buttonClear.innerText = 'Limpar dados';
+buttonClear.className = 'button-style container';
+
+document.body.appendChild(buttonClear);
+
+buttonClear.addEventListener('click', () => {
+    const firstName = document.getElementById('firstName');
+    const lastName = document.getElementById('lastName');
+    const email = document.getElementById('email');
+    const destiny = document.querySelectorAll('#destiny-favorite option');
+    const data = document.getElementById('dataTrip');
+    const textArea = document.getElementById('texto-justificativa');
+    const inputYes = document.getElementById('yes-receive');
+    const inputNo = document.getElementById('no-receive');
+    const inputAgreement = document.getElementById('agree');
+
+    firstName.value = '';
+    lastName.value = '';
+    email.value = '';
+    
+    destiny.forEach(opt => {
+        if (opt.selected == true)
+        {
+            destiny[0].selected = true;
+            return;
+        }
+    });
+
+    data.value = '';
+    textArea.value = '';
+    inputYes.checked = false;
+    inputNo.checked = false;
+    inputAgreement.checked = false;
+});
